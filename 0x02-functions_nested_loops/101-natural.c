269f4f720cc02fdee0bb1a5b29e96cda93ba6cea
adelberzi <aaberzi@gmail.com>
2023-08-15 12:04:42 -0700

    loop

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,3 +1,4 @@
+
 /*
  * File: 101-natural.c
  * Auth: vincent kip
@@ -11,18 +12,18 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-int i, sum = 0;
+	int i, sum = 0;
 
-for (i = 1; i < 1024; i++)
-{
-if (i % 3 == 0 || i % 5 == 0)
-sum += i;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
 
-printf("%d\n", sum);
-
-return 0;
-}
