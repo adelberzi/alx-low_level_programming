81a19000a0936b18196ddde22514368caec0f61b
adelberzi <aaberzi@gmail.com>
2023-08-17 15:00:06 -0700

    Nested Loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+` to start from 0 instead of 1, and modified#include "main.h"
+
+/**
+ * print_line the condition in the inner loop to ` - draws a straight line in thei < n` instead of `i <= terminal
+ * @n: number of times the character _ should be printed
+ */
+ n`. This will still produce the same output of printing the character `_`void print_line(int n)
+{
+int i;
+
+for ( `n` number of times, followed by a newlinei = 0; i character. Let me know if you have any further questions or if there's anything < n; i++)
+{
+_putchar('_');
+ else I can assist you with!}
+_putchar('\n');
+}
+
