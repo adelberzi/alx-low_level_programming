e2368145bfd5f3fd16310f484e21afa6f51e323b
adelberzi <aaberzi@gmail.com>
2023-08-17 15:57:15 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,18 +6,13 @@
  */
 int main(void)
 {
-    int i;
-    
-    printf("Infinite loop incoming :(\n");
-    i = 0;
-    
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-    
-    printf("\nInfinite loop avoided! \\o/\n");
-    
-    return (0);
+int i;    
+printf("Infinite loop incoming :(\n");
+i = 0;    
+for (i = 0; i < 10; i++)
+{       
+putchar(i + '0');
 }
-
+printf("\nInfinite loop avoided! \\o/\n");   
+return (0);
+}
