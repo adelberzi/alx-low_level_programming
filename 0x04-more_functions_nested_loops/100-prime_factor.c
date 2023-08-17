c8fc31558307e9f611eb344aab66deebbe538d58
adelberzi <aaberzi@gmail.com>
2023-08-17 15:13:43 -0700

    Nested Loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,33 +8,30 @@
  */
 int main(void)
 {
-long long n;
-long long max;
-long long i;
-
-n = 612852475143;
-max = -1;
+long n = 612852475143;
+long max = -1;
+long i;
 
 while (n % 2 == 0)
 {
 max = 2;
-n /= 2;
+n = n / 2;
 }
 
-for (i = 3; i <= sqrt(n); i += 2)
+for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
 {
 max = i;
-n /= i;
+n = n / i;
 }
 }
 
 if (n > 2)
 max = n;
 
-printf("%lld\n", max);
+printf("%ld\n", max);
 
-return 0;
+return (0);
 }
 
