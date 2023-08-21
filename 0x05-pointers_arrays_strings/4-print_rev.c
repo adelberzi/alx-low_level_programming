af79d392a25dddc11b0fb17ee8d058297ca245f4
adelberzi <aaberzi@gmail.com>
2023-08-21 10:27:39 -0700

    C-coding

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <iostream>
+/**
+ * print_rev - prints in reverse
+ * @s: string
+ * return: 0
+ */
+int main() 
+{
+int num1 = 5;
+int num2 = 10;
+int sum = num1 + num2;
+std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+return 0;
+}
