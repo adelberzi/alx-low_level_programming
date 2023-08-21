2fbb4d06776f66d6bd6c48c56941ad192ce1556e
adelberzi <aaberzi@gmail.com>
2023-08-21 11:31:37 -0700

    C-coding

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,22 +5,21 @@
 int main(void)
 {
     int password[100];
-    int i, sum, n;
+    int i, totalSum, n;
 
-    sum = 0;
+    totalSum = 0;
 
     srand(time(NULL));
 
     for (i = 0; i < 100; i++)
     {
         password[i] = rand() % 78;
-        sum += password[i];
+        totalSum += (password[i] + '0');
         putchar(password[i] + '0');
-        
-        if ((2772 - sum) < 78)
+        if ((2772 - totalSum) - '0' < 78)
         {
-            n = 2772 - sum;
-            sum += n;
+            n = 2772 - totalSum - '0';
+            totalSum += n;
             putchar(n + '0');
             break;
         }
