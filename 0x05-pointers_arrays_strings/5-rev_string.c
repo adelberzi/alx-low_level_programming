d9125ca091bfcfbc346e45c390288f39c7cd186b
adelberzi <aaberzi@gmail.com>
2023-08-21 11:07:09 -0700

    C-coding

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rev_string - Reverses a string
  * @s: Input string
@@ -7,24 +8,18 @@
 
 void rev_string(char *s)
 {
-char rev = s[0];
-int counter = 0;
-int i;
-while (s[counter] != '\0')
-counter++;
-for (i = 0; i < counter; i++)
-{
-counter--;
-rev = s[i];
-s[i] = s[counter];
-s[counter] = rev;
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
-}
-int main()
-{
-char str[] = "Hello, World!";
-printf("Original string: %s\n", str);
-rev_string(str);
-printf("Reversed string: %s\n", str);
-return 0;
-}
+
