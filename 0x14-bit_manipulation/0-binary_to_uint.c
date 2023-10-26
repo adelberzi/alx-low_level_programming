b569b978c3ca93580fb70fede359fb6564e2cad8
davidbassemm <davidbassem694@gmail.com>
2023-10-26 16:57:27 -0400

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,24 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
+ * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *x)
 {
-        int a;
-        unsigned int num;
+	int i;
+	unsigned int dec = 0;
 
-        num = 0;
-        if (!b)
-                return (0);
-        for (a = 0; b[a] != '\0'; a++)
-        {
-                if (b[a] != '0' && b[a] != '1')
-                        return (0);
-        }
-        for (a = 0; b[a] != '\0'; a++)
-        {
-                num <<= 1;
-                if (b[a] == '1')
-                        num += 1;
-        }
-        return (num);
+	if (!x)
+		return (0);
+
+	for (i = 0; x[i]; i++)
+	{
+		if (x[i] < '0' || x[i] > '1')
+			return (0);
+	}
+
+	return (dec);
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,1 @@
-Bit Manipulation project
+REAADME
